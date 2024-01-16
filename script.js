2958653f0012e1b4309837fa0ad02dabfcbeae99
sandeep-petwal <sanjupetwal51@gmail.com>
2024-01-16 18:54:25 +0530

    check

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// window.alert("hello"); 
 //onclick on loader
 loaderClick = function () {
     alert("hey 👋🏻, Thanks for visiting the site ❤️")
 }
-insta = function(){
+insta = function () {
     // window.location.href = "https://t.me/sanju_petwal";
     window.open('https://www.instagram.com/sanju.petwal', '_blank');
 }
+
+
 // Example using the Quotable API
 const apiUrl = "https://api.quotable.io/random?tags=motivational";
 
@@ -28,27 +29,36 @@ fetch(apiUrl)
 
 
 // view count local 
-    // Function to update and display the visit count
-    function updateVisitCount() {
-        // Check if localStorage is supported
-        if (typeof(Storage) !== "undefined") {
-          // Retrieve the current visit count or set it to 0 if not present
-          let visitCount = localStorage.getItem("visitCount") || 0;
-  
-          // Increment the visit count
-          visitCount++;
-  
-          // Update the count in local storage
-          localStorage.setItem("visitCount", visitCount);
-  
-          // Display the visit count on the page
-          document.querySelector('.visited_count').textContent = visitCount;
-        } else {
-          // Browser doesn't support local storage
-          document.querySelector('.visited_count').textContent = "N/A";
-        }
-      }
-  
-      // Call the updateVisitCount function when the page loads
-      window.onload = updateVisitCount;
+// Function to update and display the visit count
+function updateVisitCount() {
+    // Check if localStorage is supported
+    if (typeof (Storage) !== "undefined") {
+        // Retrieve the current visit count or set it to 0 if not present
+        let visitCount = localStorage.getItem("visitCount") || 0;
 
+        // Increment the visit count
+        visitCount++;
+
+        // Update the count in local storage
+        localStorage.setItem("visitCount", visitCount);
+
+        // Display the visit count on the page
+        document.querySelector('.visited_count').textContent = visitCount;
+    } else {
+        // Browser doesn't support local storage
+        document.querySelector('.visited_count').textContent = "N/A";
+    }
+}
+
+// Call the updateVisitCount function when the page loads
+window.onload = updateVisitCount;
+
+
+// click 
+document.addEventListener('DOMContentLoaded', function () {
+    const blinkElement = document.querySelector('.blink');
+
+    setInterval(function () {
+      blinkElement.style.visibility = (blinkElement.style.visibility === 'visible') ? 'hidden' : 'visible';
+    }, 600);
+  });
